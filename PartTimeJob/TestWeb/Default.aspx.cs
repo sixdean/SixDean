b626fceedb9f227d51c2d35ae5b3a749e48fe5dc
sixdean <wangliping89105@126.com>
2015-05-21 21:28:09 +0800

    Revert "小测试"
    This reverts commit 6e8cb7709ff2471d805fabcb0d65951b1e5e5038.

--- PartTimeJob/TestWeb/Default.aspx.cs
+++ PartTimeJob/TestWeb/Default.aspx.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 using System.Web.UI;
- 
+using TestWeb.Common;
+
 namespace TestWeb
 {
     public partial class _Default : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            var entities = new Database1Entities();
+            var sysMenu = new SysMenu
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "test",
+                ParentId = "1307311605187265267d33f281da3"
+            };
+            try
+            {
+                entities.SysMenu.AddObject(sysMenu);
+                entities.AddToSysMenu(sysMenu);
+                var i = entities.SaveChanges();
+            }
+            catch (Exception ee)
+            {
+
+                throw;
+            }
         }
     }
 }
